e889a1c81378f78d3c1f1efa48b285ac0b13e705
rongal74 <aaron.galag@gmail.com>
2024-01-31 09:08:48 +0700

    update 002/366

---
+++ 002-ReverseSequence.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+// const reverseSeq = (n) => {
+//   result = []
+//   for(let i=0; i<n; i++) {
+//     result.push(n-i)
+//   }
+//   return result;
+// };
+
+const reverseSeq = (n) =>
+  Array(n)
+    .fill(0)
+    .map((e, i) => n - i);
+
+console.log(reverseSeq(5));
