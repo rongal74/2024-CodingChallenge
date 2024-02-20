a1ef73255cb3fee230913a8364f1066681e12b2d
rongal74 <aaron.galag@gmail.com>
2024-02-20 11:22:30 +0700

    Update 011/366

---
+++ 011-RockPaperScissors.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+
+const rps = (p1, p2) => {
+  const choices = ["rock", "paper", "scissors"];
+  if (!choices.includes(p1))
+    return "Invalid move! Please play rock, paper or scissors.";
+  else if (!choices.includes(p2))
+    return `Player 2 made an invalid move: ${p2}.`;
+  let result;
+  console.log(`${p1}${p2}`);
+  switch (`${p1}${p2}`) {
+    case "rockscissors":
+    case "scissorspaper":
+    case "paperrock":
+      result = "Player 1 won!";
+      break;
+    case "scissorsrock":
+    case "rockpaper":
+    case "paperscissors":
+      result = "Player 2 won!";
+      break;
+    default:
+      result = "Draw!";
+  }
+  return result;
+};
+
+console.log(rps("rock", "scissors"));
+console.log(rps("scissors", "rock"));
+console.log(rps("paper", "paper"));
