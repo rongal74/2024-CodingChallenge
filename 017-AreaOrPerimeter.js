19ca75e03420b590bf8cb3d54d6cca6cfcc5e3f0
IKA <ika@IKAs-MacBook-Air.local>
2024-03-03 15:28:59 +0700

    Update 017/366

---
+++ 017-AreaOrPerimeter.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+
+const areaOrPerimeter = (l, w) => (l == w ? l * w : 2 * (l + w));
+
+console.log(areaOrPerimeter(4, 4));
+console.log(areaOrPerimeter(4, 5));
